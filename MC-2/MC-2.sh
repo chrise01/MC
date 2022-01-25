2ab946f7b7f04e2bdf08ce38ac0553f48f1d5d99
Chris <50994413+chrise01@users.noreply.github.com>
2022-01-25 14:34:26 +1030

    w

--- MC-2/MC-2.sh
+++ MC-2/MC-2.sh
@@ -2,6 +2,6 @@
 
 #!/bin/sh
 #cd /MC/MC-2
-docker build  -t bedrock-1.18 .
+docker build  -t bedrock-1.18-2 .
 docker volume create bedrock-server-1.18-data-2
-docker run -d -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-2,target=/minecraft-server-data-2 bedrock-1.18
+docker run -d -it -p 192.168.10.39:19132:19132/udp --mount source=bedrock-server-1.18-data-2,target=/minecraft-server-data-2 bedrock-1.18-2

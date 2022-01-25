8d14d7d5f538fa0d37013c9da87fec3a4d80fd86
Chris <50994413+chrise01@users.noreply.github.com>
2022-01-25 14:16:04 +1030

    werwe

--- MC-2.sh
+++ MC-2.sh
@@ -1,28 +1,7 @@
-https://github.com/chrise01/MC.git
 
 
-
-# - Dropped the --tag??
-MC-1
-docker build  https://github.com/chrise01/MC/MC-1/Dockerfile.git -t bedrock-1.18 .
-docker volume create bedrock-server-1.18-data-1
+#!/bin/sh
+cd /MC/MC-2
+docker build  -t bedrock-1.18 .
 docker volume create bedrock-server-1.18-data-2
-#
-docker run -d -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-1,target=/minecraft-server-data bedrock-1.18
-#
-docker run -d -it -p 192.168.10.39:19132:19132/udp --mount source=bedrock-server-1.18-data-2,target=/minecraft-server-data bedrock-1.18
-
-https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.2.03.zip
-
-
-https://blog.ycshao.com/2020/05/17/how-to-set-up-minecraft-bedrock-server-in-docker/
-
-Run
-
-#List running containers - need Container ID
-docker ps
-#Attach container?
-docker attach 81ca26163d14
-
-192.168.10.29
-192.168.10.49
+docker run -d -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-2,target=/minecraft-server-data-2 bedrock-1.18

b0d88b493acdaddceb8d1b9e18408e5abbb72207
Chris <50994413+chrise01@users.noreply.github.com>
2022-01-25 18:08:21 +1030

    Stupid Git

--- MC-1/MC-1.sh
+++ MC-1/MC-1.sh
@@ -4,4 +4,4 @@
 #cd /MC-1
 docker build  -t bedrock-1.18 .
 docker volume create bedrock-server-1.18-data-1
-docker run -d -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-1,target=/minecraft-server-data-1 bedrock-1.18
+docker run -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-1,target=/minecraft-server-data-1 bedrock-1.18

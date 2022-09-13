f9e177219da55b8904db7e29226dce799301cebe
Chris <50994413+chrise01@users.noreply.github.com>
2022-09-13 11:19:48 +0930

    broken

--- MC-1/MC-1.sh
+++ MC-1/MC-1.sh
@@ -2,6 +2,7 @@
 
 #!/bin/sh
 #cd /MC-1
-docker build  -t bedrock-1.18 .
+snap install docker
+docker build  -t bedrock-1.19 .
 docker volume create bedrock-server-1.18-data-1
-docker run -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.18-data-1,target=/minecraft-server-data-1 bedrock-1.18
+docker run -it -p 192.168.10.29:19132:19132/udp --mount source=bedrock-server-1.19-data-1,target=/minecraft-server-data-1 bedrock-1.19
